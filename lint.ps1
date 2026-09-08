c5513608a8cf0252efd37cde06f6415dce1de6b4
jay <jay079700@gmail.com>
2026-09-08 19:55:01 +0700

    Presentation

--- lint.ps1
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-mypy .
-ruff check . --fix 
-ruff format .
-autoflake --in-place --remove-all-unused-imports --remove-unused-variables --remove-duplicate-keys --recursive .
--- src/domain/entity_logic/move_order.py
+++
@@ -1,8 +0,0 @@
-from domain.base.core.character import Character
-from domain.base.utils.point import Point
-
-
-class MoveOrder:
-    def __init__(self, movable_entity: Character, target_point: Point) -> None:
-        self.movable_entity = movable_entity
-        self.target_point = target_point
